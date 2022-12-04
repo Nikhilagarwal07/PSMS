727ff9705c5a3961fd639115ff322ddf22f5dffa
ayush-sb <ayush.sb2003@gmail.com>
2022-12-04 16:35:28 +0530

    reenable clearScreen()

--- src/App.java
+++ src/App.java
@@ -11,8 +11,8 @@ public class App {
     }
 
     public static void clearScreen() {  
-        // System.out.print("\033[H\033[2J");  
-        // System.out.flush();
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();
     }
 
     public static void main(String[] args) throws Exception {
